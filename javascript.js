4ddd3ac3dd9aef63820a118162ec4ba21ada4d80
Loam Shin <loamshin@Loams-MacBook-Air.local>
2022-03-12 11:50:16 -0500

    website

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function navBarFunction() {
+    document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
+}
+
+
+function navBarFunction() {
+    document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
+}
+
+function navBarFunction() {
+    document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
+}
+
+function navBarFunction() {
+    document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
+}
+$('#toggle').click(function() {
+   $(this).toggleClass('active');
+   $('#overlay').toggleClass('open');
+  });
